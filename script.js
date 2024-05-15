a8b68d044c10620e563d1b02e9764a931cb90ce1
tanatut12 <tanatut12@gmail.com>
2024-05-15 13:40:30 +0700

    finish

---
+++ script.js
@@ -0,0 +1,18 @@
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+console.log(myAlphabet.length);
+
+const length = function() {
+    console.log(myAlphabet.length);
+}
+
+let Planets = ["Mercury","venus","earth","mars","jupiter"];
+Planets.forEach(function(a,b) {
+    console.log(a,b)
+} )
+
+let myArr = [ 1, 2, 'One', true]; 
+
+myArr.forEach(function(myArr) {
+    console.log(myArr)
+})
+
